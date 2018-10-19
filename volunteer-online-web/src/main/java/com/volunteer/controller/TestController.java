aa7c54947868a095bc1e99fbe5fec9f75743046b
啦啦啦 <820031877@qq.com>
2018-10-19 10:04:08 +0800

    志愿者服务平台

---
+++ volunteer-online-web/src/main/java/com/volunteer/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.volunteer.controller;
+
+import com.volunteer.entity.TestTable;
+import com.volunteer.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.constraints.Null;
+import java.util.List;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @RequestMapping("main")
+    @ResponseBody
+    public List<TestTable> test(){
+        List<TestTable> list = testService.getList();
+        return list;
+    }
+}
